a932da9f6bb9834d5405b066e285608c2b4c35be
CFussell <clayton.fussell@gmail.com>
2015-01-21 15:55:47 +0000

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'products/new'
+
+  get 'products/edit'
+
  # 1. change the default landing page to primary resource landing page - in this instance it will be products
  root "products#index"
  
